ac5686ef8542f3be8a67f8477118b3d58caf934f
Yifan Wu <tyivanwu@gmail.com>
2023-05-21 11:01:29 -0400

    feat: init prompt library

--- ml_playground/ask_gpt/cli.py
+++ ml_playground/ask_gpt/cli.py
@@ -4,6 +4,8 @@ from functools import partial
 import click
 import openai
 
+from ml_playground.ask_gpt.prompt import Template, get_template
+
 click.option = partial(click.option, show_default=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -21,17 +23,35 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0, stream=True):
     return response.choices[0].message["content"]
 
 
+def compile_prompt(prompt, language, template):
+    _template = get_template(template)
+    return _template.format(prompt=prompt, language=language) if _template else prompt
+
+
 @click.command()
 @click.argument("prompt", required=True)
 @click.option("--model", default="gpt-3.5-turbo", help="Model of GPT.")
 @click.option(
-    "--temperature", default=0.8, help="Degree of randomness of the model's output"
+    "--temperature", default=0.8, help="Degree of randomness of the model's output."
 )
 @click.option("--stream", default=True, help="Stream the GPT response.")
-def main(prompt, model, temperature, stream):
-    print(f"提问: {prompt}\n")
+@click.option(
+    "--template",
+    type=click.Choice([t.value for t in Template]),
+    help="Prompt template.",
+)
+@click.option("--language", default="English", help="Target language of answers.")
+def main(prompt, model, temperature, stream, template, language):
+    _prompt = compile_prompt(prompt, language, template)
+    response = get_completion(
+        _prompt,
+        model,
+        temperature,
+        stream,
+    )
+
+    print(f"提问: {_prompt}\n")
     print("gpt 回答: ", end="")
-    response = get_completion(prompt, model, temperature, stream)
     if stream:
         for chunk in response:
             if content := chunk["choices"][0].get("delta").get("content"):
---
+++ ml_playground/ask_gpt/prompt.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+template_socratic_questioning = """
+Pretend you are GPT-4 model. Your task is to automatically take turns asking and answering questions. We'll begin with an initial question. Then go on answering and asking in this pattern:
+Question: The initial question
+Answer: The answer to the initial question
+Question: The question about the reason for the previous answer
+Answer: The answer to the previous question
+Question:
+Keep asking about the origin of the thinking, alternative viewpoints and perspectives and conflicts between contentions, exploring implications and consequences, questioning the question for the last answer. Stop only when the answer is "That's the way it is" or "We don't know for now". Each question and answer should be a single sentence with no more than 20 words. Add "Q: " before each question and "A: " before each answer.
+
+Ask and answer in '{language}' regardless of the language I use. Don't show the translation process. Just write questions and answers in the destination language.
+
+Now, the initial question is: '{prompt}'
+"""
+
+template_three_questions = """
+Pretend you are GPT-4 model. Whenever I ask you about a piece of knowledge, you should raise three questions and try to answer these three questions.
+These three questions should be asked according to the following ideas:
+1. Where does it come from? This question implies that the emergence of knowledge is not created out of thin air; it must have been born to solve a problem.
+2. What is it? This question implies what kind of knowledge it is itself. What solutions does it propose for the problem it aims to solve?
+3. Where is it going? This question implies what flaws exist in the knowledge itself regarding the resolution of the problem? What are its limitations? What is the future direction of development?
+
+Ask and answer in '{language}' regardless of the language I use. Don't show the translation process. Just write questions and answers in the destination language.
+
+Now, the initial question is: '{prompt}'
+"""
+
+
+class Template(Enum):
+    SOCRATIC = "socratic"
+    THREE = "three"
+
+
+template_map = {
+    Template.SOCRATIC.value: template_socratic_questioning,
+    Template.THREE.value: template_three_questions,
+}
+
+
+def get_template(name: str) -> str:
+    return template_map.get(name)
