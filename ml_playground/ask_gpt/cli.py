daaed59ed55f9f750ea7c4f34e1a18c2ab016050
Yifan Wu <tyivanwu@gmail.com>
2023-05-21 10:04:20 -0400

    refactor: abstract ask_gpt package

---
+++ ml_playground/ask_gpt/cli.py
@@ -0,0 +1,44 @@
+import os
+from functools import partial
+
+import click
+import openai
+
+click.option = partial(click.option, show_default=True)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0, stream=True):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        stream=stream,
+    )
+    if stream:
+        return response
+    return response.choices[0].message["content"]
+
+
+@click.command()
+@click.argument("prompt", required=True)
+@click.option("--model", default="gpt-3.5-turbo", help="Model of GPT.")
+@click.option(
+    "--temperature", default=0.8, help="Degree of randomness of the model's output"
+)
+@click.option("--stream", default=True, help="Stream the GPT response.")
+def main(prompt, model, temperature, stream):
+    print(f"提问: {prompt}\n")
+    print("gpt 回答: ", end="")
+    response = get_completion(prompt, model, temperature, stream)
+    if stream:
+        for chunk in response:
+            if content := chunk["choices"][0].get("delta").get("content"):
+                print(content, end="", flush=True)
+    else:
+        print(response)
+
+
+if __name__ == "__main__":
+    main()
